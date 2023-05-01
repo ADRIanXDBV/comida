5a60af4052530390734c40c898d8a6ff29a8cf56
ADRIanXDBV <adriansito_a@hotmail.com>
2023-05-01 10:28:21 -0500

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+   firebaseConfig : {
+    apiKey: "AIzaSyAa2hV11gAY234Xq7UC1FZ7NUZSGK9zsDU",
+    authDomain: "comida-522d2.firebaseapp.com",
+    projectId: "comida-522d2",
+    storageBucket: "comida-522d2.appspot.com",
+    messagingSenderId: "823420960171",
+    appId: "1:823420960171:web:2bbf8081eca5d2f997e6ee"
+  } 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAa2hV11gAY234Xq7UC1FZ7NUZSGK9zsDU",
+    authDomain: "comida-522d2.firebaseapp.com",
+    projectId: "comida-522d2",
+    storageBucket: "comida-522d2.appspot.com",
+    messagingSenderId: "823420960171",
+    appId: "1:823420960171:web:2bbf8081eca5d2f997e6ee"
+  }
 };
 
 /*
