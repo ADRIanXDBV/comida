d57978751db4dba0319a8aeaa67bf686fd56b22c
ADRIanXDBV <adriansito_a@hotmail.com>
2023-05-02 22:56:51 -0500

    nose

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
+
+
    firebaseConfig : {
     apiKey: "AIzaSyAa2hV11gAY234Xq7UC1FZ7NUZSGK9zsDU",
     authDomain: "comida-522d2.firebaseapp.com",
@@ -7,5 +9,5 @@ export const environment = {
     storageBucket: "comida-522d2.appspot.com",
     messagingSenderId: "823420960171",
     appId: "1:823420960171:web:2bbf8081eca5d2f997e6ee"
-  } 
+  }
 };
